cc8d9507bdbbe48279c815f368ea05a8fb254998
AndrewDPohl <andrewdpohl@gmail.com>
2021-05-09 22:17:18 -0700

    Added updated tests...
    * etc etc etc

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# seesawAssessment
-Seesaw Assessment
+# Seesaw Assessment
+
+## Getting Started
+
+### Welcome to my Seesaw Automation Testing Take Home Assessment.  I hope you find it to meet and exceed your expectations.
+
+
+1. First and foremost, copy and clone this repository and add it to your local machine in the directory of your choosing
+
+2. If you do not have Node or NPM set up on your machine, you can use [the following tutorial](https://blog.teamtreehouse.com/install-node-js-npm-mac) for Mac
+
+2. After you've added it to your local machine, run the following command to get your testing framework ready and install all of the necessary packages
+```
+npm install
+``` 
+
+## How To Run Your Tests
+
+[Cypress](https://docs.cypress.io/guides/overview/why-cypress) is the testing framework that I chose for my testing. You can either run them from your command line, or if you prefer, Cypress offers a handy console that you can open up to run the tests, and utilize many built in tools for inspecting and visualizing the output of the test
+
+To open up the Cypress Console, run: 
+```
+npm run cy:open
+```
+	
+And wait for it to populate the console.  From there, you can either run each test on it's own, or run all tests available.  Though we are not testing any UI automation, one of the other great features of Cypress is that you can also choose from the selection of browsers that are currently available on your machine that are compatible.
+
+To run a test from the command line, while you're in the "zwift" directory, run:
+```
+npm run cy:run
+```
+This will run all tests and provide output in your terminal
+
+
+## Why I Chose Cypress
+
+Cypress is a new(ish) testing framework that seems to have become the standard for many engineering teams as it provides virtually everything you'd need out of the box to get end-to-end tests going.  It comes with a handy UI console that allows you to run and view your tests in real time, and has additional features such as step-by-step viewing of your tests with visual context for each step, the ability to change which browser you are testing on the fly, open up your IDE to the code that is being used during any particular test straight from the console, and more.
+
+Cypress has a great deal of well written documentation and a nice community for support.
---
+++ cypress/integration/seesaw/create_student.spec.js
@@ -0,0 +1,110 @@
+const faker = require('faker');
+
+const studentData = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    phoneNumber: faker.phone.phoneNumber(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    studentOrgId: faker.random.number(),
+    orgId: 'org.94961013-f682-41c8-b635-4c117c5b0565',
+    fakeOrgId: 'org.xxxxxxxxxxxxxxxxxxxxxxxxx'
+}
+
+const schoolAdminData = {
+    email: 'qa+ashbyadmin@seesaw.me',
+    password: 'qa-take-home',
+    adminName: 'Allen Ashby'
+}
+
+const BASEURL = 'https://app.seesaw.me/api'
+
+describe('Creation of a Student in the Seesaw Application', function () {
+
+    let userToken = "";
+    let studentFullName = "";
+
+    it('Should be able to log in as a school administrator', () => {
+        cy.request({
+            method: 'POST',
+            url: BASEURL + '/auth/login',
+            form: true,
+            body: {
+                email: schoolAdminData.email,
+                password: schoolAdminData.password
+            }
+        }).then((data) => {
+            userToken = data.body.response.user_token;
+            expect(data.status).to.eq(200);
+            expect(data.body.response.person.display_name).to.eq(schoolAdminData.adminName);
+            // cy.log(JSON.stringify(data));
+        })
+    })
+
+    it('Should be able to get a full list of students', () => {
+        cy.request({
+            method: 'GET',
+            url: BASEURL + '/org/student_list_v2',
+            auth: {
+                bearer: userToken
+            },
+            qs: {
+                org_id: studentData.orgId
+            }
+        }).then((data) => {
+            let studentListTotal = data.body.response.students.length;
+            cy.wrap(studentListTotal).should('be.gt', 0);
+            expect(data.status).to.eq(200);
+            // cy.log(JSON.stringify(data.body.response.students));
+        })
+    })
+
+    it('Should be able to create a new student', () => {
+        cy.request({
+            method: 'POST',
+            url: BASEURL + '/org/create_org_student',
+            auth: {
+                bearer: userToken
+            },
+            qs: {
+                org_id: studentData.orgId,
+                student_org_id: studentData.studentOrgId,
+                first_name: studentData.firstName,
+                last_name: studentData.lastName,
+                email: studentData.email,
+                password: studentData.password
+            }
+        }).then((data) => {
+            studentFullName = studentData.firstName + " " + studentData.lastName;
+            expect(data.status).to.eq(200)
+            expect(data.body.response.student.display_name).to.eq(studentFullName);
+            expect(data.body.response.student.email.toUpperCase()).to.eq(studentData.email.toUpperCase());
+            // cy.log(JSON.stringify(data.body))
+        })
+    })
+
+    it('Should be NOT be able to create a new student if an invalid Org ID is passed', () => {
+        cy.request({
+            method: 'POST',
+            url: BASEURL + '/org/create_org_student',
+            auth: {
+                bearer: userToken
+            },
+            qs: {
+                org_id: studentData.fakeOrgId,
+                student_org_id: studentData.studentOrgId,
+                first_name: studentData.firstName,
+                last_name: studentData.lastName,
+                email: studentData.email,
+                password: studentData.password
+            },
+            failOnStatusCode: false
+        }).then((data) => {
+            expect(data.status).not.to.eq(200);
+            expect(data.status).to.eq(403);
+            expect(data.statusText).to.eq('Forbidden');
+        })
+    })
+
+
+})
